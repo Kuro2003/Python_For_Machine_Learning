1143757c28584c7a037fbdbf3a94b6a55e594ca6
Kuro2003 <cuongnguyengl2003@gmail.com>
2022-07-14 20:37:01 +0700

    update and delete folder

--- Game_Basic/alien_invasion/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    # A class to store all settings for Alien Invasion.
-    # Screen settings
-    def __init__(self):
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (255,255,230)
-        self.ship_speed = 1.5
